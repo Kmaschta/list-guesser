3189f7bff38d12e9d51b752d7030b353ba404dac
Kmaschta <kevin@marmelab.com>
2019-06-12 14:46:36 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const IgnoreNotFoundExportPlugin = require('ignore-not-found-export-plugin');
+
+module.exports = {
+    devtool: 'cheap-module-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(t|j)sx?$/,
+                exclude: /node_modules/,
+                use: { loader: 'babel-loader' },
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                use: { loader: 'html-loader' },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new HardSourceWebpackPlugin(),
+        // required because of https://github.com/babel/babel/issues/7640
+        new IgnoreNotFoundExportPlugin([
+            'CallbackSideEffect',
+            'NotificationSideEffect',
+            'RedirectionSideEffect',
+            'RefreshSideEffect',
+        ]),
+    ],
+    devServer: {
+        stats: {
+            children: false,
+            chunks: false,
+            modules: false,
+        },
+    },
+};
