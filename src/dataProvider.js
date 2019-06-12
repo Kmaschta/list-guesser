18d3b551465b11c2d835f40987f0b562008eaca6
Kmaschta <kevin@marmelab.com>
2019-06-12 16:49:19 +0200

    Fix Hydra ids

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -13,6 +13,19 @@ const enhancedDataProvider = (type, resource, params) => {
         return getApiSchema(ENTRYPOINT);
     }
 
+    if (type === 'GET_LIST') {
+        return dataProvider(type, resource, params).then(({ data, total }) => ({
+            data: data ? data.map(item => ({ ...item, id: item['@id'] })) : data,
+            total
+        }));
+    }
+
+    if (type === 'GET_ONE') {
+        return dataProvider(type, resource, params).then(({ data }) => ({
+            data: data ? { ...data, id: data['@id'] } : data
+        }));
+    }
+
     return dataProvider(type, resource, params)
 }
 
